c06039290fea996f3886792a361a0294863d34b6
Luciano Leveroni <lleveroni@eryx.co>
2018-01-31 00:56:47 -0300

    Allow remote connections when running in development mode.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from config import Config
 from factory import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    if Config.DEVELOPMENT == 'True':
+        app.run(host='0.0.0.0')
+    else:
+        app.run()
